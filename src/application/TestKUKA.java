f1794e660f6a6c23de7a3b0f9569160d9b48f7e8
azmani <azmani@VGPHAZMANI2025.kuka.int.kuka.com>
2025-11-03 17:59:46 +0100

    --- automatic rebase to remote; old local SHA: 66e2d29e4c9aff42acfc26cd641cfe6375f6f6e7 ---

---
+++ src/application/TestKUKA.java
@@ -0,0 +1,55 @@
+package application;
+
+
+import javax.inject.Inject;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.motionModel.PTP;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class TestKUKA extends RoboticsAPIApplication {
+	@Inject
+	private LBR lbr;
+	private PTP P_inicio = ptp(Math.toRadians(36), Math.toRadians(-47), Math.toRadians(10), Math.toRadians(-54), Math.toRadians(-78), Math.toRadians(97), Math.toRadians(-140));
+	private PTP P_final = ptp(Math.toRadians(25), Math.toRadians(47), Math.toRadians(62), Math.toRadians(90), Math.toRadians(-5), Math.toRadians(-72), Math.toRadians(-61));
+	@Override
+	public void initialize() {
+		// initialize your application here
+		 
+		P_inicio.setJointVelocityRel(0.25);
+		P_final.setJointVelocityRel(0.25);
+
+	}
+
+	@Override
+	public void run() {
+		// your application execution starts here
+		
+		while(true)
+		{
+			lbr.move(P_inicio);
+			lbr.move(P_final);
+		}
+		
+		
+	}
+}
