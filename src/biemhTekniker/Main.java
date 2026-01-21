944109171234d48a4188e8d3b9f96d23fef862ab
Alexander Sebastian Kalis <alex.kalis@outlook.com>
2026-01-21 14:54:27 +0100

    --- automatic rebase to remote; old local SHA: 0143598f3e64275b46882034a5d450d0e14464dd ---

--- src/biemhTekniker/Main.java
+++ src/biemhTekniker/Main.java
@@ -20,7 +20,15 @@ public class Main extends RoboticsAPIApplication
     @Override
     public void run()
     {
-
+    	while(true)
+    	{
+    		try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
     }
 
     @Override
