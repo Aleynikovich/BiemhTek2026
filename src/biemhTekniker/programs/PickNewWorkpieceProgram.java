7b0c0137d8d1200e8450bae38f6349a7b821dffd
Alexander Sebastian Kalis <alex.kalis@outlook.com>
2026-02-02 17:35:02 +0100

    --- automatic rebase to remote; old local SHA: f02ca5d61950f55a72575bebeed7e13e6ca8d502 ---

--- src/biemhTekniker/programs/PickNewWorkpieceProgram.java
+++ src/biemhTekniker/programs/PickNewWorkpieceProgram.java
@@ -63,7 +63,7 @@ public class PickNewWorkpieceProgram {
         
         log.debug("Using workpiece position: " + workpieceData.toString());
         
-        ObjectFrame tcp = gripper.getFrame("TCPB");
+        ObjectFrame tcp = gripper.getFrame("TCPA");
         Frame pickPosition = workpieceData.getWorkPiecePickFrame();
         Frame prePickPosition = workpieceData.getWorkPiecePickFrame();
         Frame rotatePosition = workpieceData.getWorkPiecePickFrame();
