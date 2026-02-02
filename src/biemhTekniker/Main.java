ec49a24fca91829c3942fdf8b94b778df51cf2c7
Alexander Sebastian Kalis <alex.kalis@outlook.com>
2026-02-02 12:12:03 +0100

    --- automatic commit on synchronization ---

--- src/biemhTekniker/Main.java
+++ src/biemhTekniker/Main.java
@@ -246,8 +246,9 @@ public class Main extends RoboticsAPIApplication implements ConsoleServerInterfa
     	// Check if workpieceData is null; if so, create a new instance
         if (this.workpieceData == null) {
             this.workpieceData = new WorkpieceData();
+            workpieceData.set(300.0, -320, 200,-165 , 0.0,58, 0.95);
         }
-        workpieceData.set(300.0, -320, 200,-165 , 0.0,58, 0.95);
+        
         //REMOVE HARDCODE
         PickNewWorkpieceProgram program = new PickNewWorkpieceProgram(
                 this, 
