e4e0786a7fe3b4d667a1152e312d9e5ff9ea918a
Alexander Sebastian Kalis <alex.kalis@outlook.com>
2026-02-05 09:28:30 +0100

    --- automatic rebase to remote; old local SHA: be06fb2710c6fb39d52d2538f888ec53cc9e4614 ---

--- src/biemhTekniker/programs/PickNewWorkpieceProgram.java
+++ src/biemhTekniker/programs/PickNewWorkpieceProgram.java
@@ -2,7 +2,7 @@ package biemhTekniker.programs;
 
 import biemhTekniker.data.WorkpieceData;
 import biemhTekniker.logger.Logger;
-import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
+import biemhTekniker.toolcontrol.ToolControl;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.geometricModel.Frame;
@@ -24,15 +24,15 @@ public class PickNewWorkpieceProgram
     private final LBR                    iiwa;
     private final WorkpieceData          workpieceData;
     private final Tool                   gripper;
-    private final MediaFlangeIOGroup     gripperIO;
+    private final ToolControl            toolControl;
 
-    public PickNewWorkpieceProgram(RoboticsAPIApplication application, LBR robot, WorkpieceData workpieceData, Tool gripper, MediaFlangeIOGroup gripperIO)
+    public PickNewWorkpieceProgram(RoboticsAPIApplication application, LBR robot, WorkpieceData workpieceData, Tool gripper, ToolControl toolControl)
     {
         this.application   = application;
         this.iiwa          = robot;
         this.workpieceData = workpieceData;
         this.gripper       = gripper;
-        this.gripperIO     = gripperIO;
+        this.toolControl   = toolControl;
     }
 
     /**
@@ -52,15 +52,20 @@ public class PickNewWorkpieceProgram
 
         log.debug("Using workpiece position: " + workpieceData);
 
-        ObjectFrame tcp             = gripper.getFrame("TCPB");
+        ObjectFrame tcp             = gripper.getFrame("TCPA");
         Frame       pickPosition    = workpieceData.getWorkPiecePickFrame();
         Frame       prePickPosition = workpieceData.getWorkPiecePickFrame();
-        
+        Frame       rotatePosition  = workpieceData.getWorkPiecePickFrame();
         prePickPosition.setZ(prePickPosition.getZ() + 100);
+        rotatePosition.setAlphaRad(rotatePosition.getAlphaRad() + Math.toRadians(10));
+
 
         tcp.move(ptp(prePickPosition).setJointVelocityRel(0.5));
         tcp.move(lin(pickPosition).setJointVelocityRel(0.25));
-        tcp.move(lin(prePickPosition).setJointVelocityRel(0.25));
+        //tcp.move(ptp(pickPosition.transform(Transformation.ofDeg(0, 0, 100, 0, 0, 0))).setJointVelocityRel(0.5));
+        tcp.move(ptp(rotatePosition).setJointVelocityRel(0.5));
+        rotatePosition.setZ(rotatePosition.getZ() + 100);
+        tcp.move(lin(rotatePosition).setJointVelocityRel(0.25));
         gripper.move(ptp(application.getApplicationData().getFrame("/BiemhHome")));
 
         return true;
