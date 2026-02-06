e76399cad12bb749eaa8f914972851816333c593
Alexander Sebastian Kalis <alex.kalis@outlook.com>
2026-02-06 12:01:13 +0100

    --- automatic rebase to remote; old local SHA: 12af95900ea8f2767bc6793bd300b4cf85ef205c ---

--- src/biemhTekniker/Main.java
+++ src/biemhTekniker/Main.java
@@ -146,6 +146,7 @@ public class Main extends RoboticsAPIApplication implements ConsoleServerInterfa
 
                 case 2:
                     // Program 2 - Calibration
+                	iiwa.move(ptp(getApplicationData().getFrame("/HomeCalibration")));
                     executeCalibration();
                     programNumber = 0;
                     break;
