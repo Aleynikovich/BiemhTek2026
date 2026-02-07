8171e83890274bc6fd7715fa3a926a4039aabfbb
Alex Kalis <alex.kalis@outlook.com>
2026-02-07 20:58:55 +0100

    --- automatic commit on synchronization ---

---
+++ src/IOPolling/AxisPositionPolling.java
@@ -0,0 +1,37 @@
+package IOPolling;
+
+import com.kuka.generated.ioAccess.RobotJointPositionIOGroup;
+import com.kuka.roboticsAPI.applicationModel.tasks.CycleBehavior;
+import com.kuka.roboticsAPI.applicationModel.tasks.RoboticsAPICyclicBackgroundTask;
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+
+import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
+
+public class AxisPositionPolling extends RoboticsAPICyclicBackgroundTask
+{
+    private final   int                       decimalMultiplier = 10;
+    @Inject private Controller                sunrise;
+    @Inject private LBR                       iiwa;
+    @Inject private RobotJointPositionIOGroup currentAxisPosition;
+
+    @Override public void initialize()
+    {
+        // initialize your task here
+        initializeCyclic(0, 500, TimeUnit.MILLISECONDS, CycleBehavior.BestEffort);
+    }
+
+    @Override public void runCyclic()
+    {
+        try
+        {
+            currentAxisPosition.setA1((int) Math.round(iiwa.getCurrentJointPosition().get(1)));
+
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
