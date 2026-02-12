48002b5fc214fac7b73540b8b272738ef5acb4da
Alexander Sebastian Kalis <alex.kalis@outlook.com>
2026-02-12 10:51:46 +0100

    --- automatic rebase to remote; old local SHA: b161f5e9f97a8ca2c30b11e9266cffc1bf2bb647 ---

--- src/biemhTekniker/programs/robot/subprograms/PickNewWorkpieceProgram.java
+++ src/biemhTekniker/programs/robot/subprograms/PickNewWorkpieceProgram.java
@@ -119,7 +119,7 @@ public class PickNewWorkpieceProgram implements RobotProgram
                 ThreadUtil.milliSleep(GRIPPER_ACTIVATION_DELAY_MS);
             }
         };
-
+        robot.getFlange().moveAsync(ptp(app.getApplicationData().getFrame("/binCenter")).setBlendingCart(0.5));
         // Try each strategy until one succeeds
         boolean pickSucceeded = false;
         MotionStrategy successfulStrategy = null;
